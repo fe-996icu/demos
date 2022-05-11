1110f0681eee33741a05cd8226295ad61fd0ba1c
zhangjianzhong <zhangjianzhong>
2022-05-11 18:33:27 +0800

    手写JSON.stringify()，掘金

--- jsonstringify/jsonstringify.js
+++ jsonstringify/jsonstringify.js
@@ -1,3 +1,29 @@
+// 参考： https://juejin.cn/post/7017588385615200270
+
+/* JSON.stringify() 9大特性
+
+	特性一
+		1、undefined、任意的函数以及symbol值，出现在非数组对象的属性值中时在序列化过程中会被忽略
+		2、undefined、任意的函数以及symbol值出现在数组中时会被转换成 null。
+		3、undefined、任意的函数以及symbol值被单独转换时，会返回 undefined
+	特性二
+		布尔值、数字、字符串的包装对象在序列化过程中会自动转换成对应的原始值。
+	特性三
+		所有以symbol为属性键的属性都会被完全忽略掉，即便 replacer 参数中强制指定包含了它们。
+	特性四
+		NaN 和 Infinity 格式的数值及 null 都会被当做 null。
+	特性五
+		转换值如果有 toJSON() 方法，该方法定义什么值将被序列化。
+	特性六
+		Date 日期调用了 toJSON() 将其转换为了 string 字符串（同Date.toISOString()），因此会被当做字符串处理。
+	特性七
+		对包含循环引用的对象（对象之间相互引用，形成无限循环）执行此方法，会抛出错误。
+	特性八
+		其他类型的对象，包括 Map/Set/WeakMap/WeakSet，仅会序列化可枚举的属性
+	特性九
+		当尝试去转换 BigInt 类型的值会抛出错误
+*/
+
 const jsonstringify2 = (data) => {
 	// 确认一个对象是否存在循环引用
 	const isCyclic = (obj) => {
